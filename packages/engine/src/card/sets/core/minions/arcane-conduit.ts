cac88797b3f0f4fac9d1202bced86483d1532023
Darialyphia <lo.pennequin@gmail.com>
2025-08-24 02:41:53 +0200

    feat: Enhance Arcane Conduit with event listener for Arcane Spell plays

--- packages/engine/src/card/sets/core/minions/arcane-conduit.ts
+++ packages/engine/src/card/sets/core/minions/arcane-conduit.ts
@@ -1,7 +1,12 @@
+import { GAME_EVENTS } from '../../../../game/game.events';
+import { GameEventModifierMixin } from '../../../../modifier/mixins/game-event.mixin';
 import { TogglableModifierMixin } from '../../../../modifier/mixins/togglable.mixin';
+import { UntilEndOfTurnModifierMixin } from '../../../../modifier/mixins/until-end-of-turn.mixin';
+import { Modifier } from '../../../../modifier/modifier.entity';
 import { BlitzModifier } from '../../../../modifier/modifiers/blitz.modifier';
 import { SimpleAttackBuffModifier } from '../../../../modifier/modifiers/simple-attack-buff.modifier';
 import type { MinionBlueprint } from '../../../card-blueprint';
+import { isSpell } from '../../../card-utils';
 import {
   AFFINITIES,
   CARD_DECK_SOURCES,
@@ -9,12 +14,13 @@ import {
   CARD_SETS,
   RARITIES
 } from '../../../card.enums';
+import type { MinionCard } from '../../../entities/minion.entity';
 
 export const arcaneConduit: MinionBlueprint = {
   id: 'arcane-conduit',
   name: 'Arcane Conduit',
   cardIconId: 'unit-arcane-conduit',
-  description: `Whenever you play an Arcane Spell, activate this unit and give it +1@[attack]@ this turn.`,
+  description: `Whenever you play an Arcane Spell, wake up this unit and give it +1@[attack]@ this turn.`,
   collectable: true,
   unique: false,
   manaCost: 2,
@@ -28,6 +34,34 @@ export const arcaneConduit: MinionBlueprint = {
   abilities: [],
   tags: [],
   canPlay: () => true,
-  async onInit(game, card) {},
+  async onInit(game, card) {
+    await card.modifiers.add(
+      new Modifier<MinionCard>('arcane-conduit-listener', game, card, {
+        mixins: [
+          new GameEventModifierMixin(game, {
+            eventName: GAME_EVENTS.CARD_AFTER_PLAY,
+            async handler({ data }) {
+              if (!isSpell(data.card)) return;
+              if (data.card.affinity !== AFFINITIES.ARCANE) return;
+              if (!data.card.player.equals(card.player)) return;
+              await card.wakeUp();
+
+              await card.modifiers.add(
+                new SimpleAttackBuffModifier<MinionCard>(
+                  'arcane-conduit-attack-buff',
+                  game,
+                  card,
+                  {
+                    amount: 1,
+                    mixins: [new UntilEndOfTurnModifierMixin(game)]
+                  }
+                )
+              );
+            }
+          })
+        ]
+      })
+    );
+  },
   async onPlay() {}
 };
