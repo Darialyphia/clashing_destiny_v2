c18bf51dc5cfc9fa6faea3cf0a11d28c7e584b05
Darialyphia <lo.pennequin@gmail.com>
2025-10-12 00:17:15 +0200

    feat: implement welcome gifts for new accounts in DeckSubscribers

--- packages/api/src/convex/deck/deck.subscribers.ts
+++ packages/api/src/convex/deck/deck.subscribers.ts
@@ -3,7 +3,8 @@ import { eventEmitter, type EventEmitter } from '../shared/eventEmitter';
 import type { Scheduler } from 'convex/server';
 import { premadeDecks } from './premadeDecks';
 import { internal } from '../_generated/api';
-
+import { CARDS_DICTIONARY } from '@game/engine/src/card/sets';
+import { GIFT_KINDS, GIFT_SOURCES } from '../gift/gift.constants';
 export class DeckSubscribers {
   static INJECTION_KEY = 'deckSubscribers' as const;
 
@@ -15,10 +16,35 @@ export class DeckSubscribers {
     const starterDecks = premadeDecks.filter(deck => deck.isGrantedOnAccountCreation);
 
     for (const deck of starterDecks) {
-      await this.ctx.scheduler.runAfter(0, internal.decks.grantPremadeDeck, {
-        userId: event.userId,
-        premadeDeckId: deck.id
+      await this.ctx.scheduler.runAfter(0, internal.gifts.give, {
+        receiverId: event.userId,
+        name: `Welcome Gift: ${deck.name}`,
+        source: GIFT_SOURCES.SIGNUP_GIFT,
+        contents: [
+          {
+            kind: GIFT_KINDS.DECK,
+            deckId: deck.id
+          }
+        ]
       });
     }
+
+    await this.ctx.scheduler.runAfter(0, internal.gifts.give, {
+      receiverId: event.userId,
+      name: 'Pre Alpha Welcome Gift: Full collection unlock !',
+      source: GIFT_SOURCES.PROMOTION,
+      contents: [
+        {
+          kind: GIFT_KINDS.CARDS,
+          cards: Object.values(CARDS_DICTIONARY)
+            .filter(card => card.collectable)
+            .map(card => ({
+              blueprintId: card.id,
+              isFoil: false,
+              amount: 4
+            }))
+        }
+      ]
+    });
   }
 }
